f5364873922634c5442cc50f43f42e9b92a0e09f
Kevin Zeledón <magneticok@gmail.com>
2022-09-18 12:51:05 -0600

    Agregando estructuras basicas

---
+++ common/include/types.h
@@ -0,0 +1,21 @@
+#include <stdbool.h>
+
+typedef struct Pixel {
+	int valor;
+	int index;
+	int metadata_id;
+	bool dirtyBit;
+} Pixel_t;
+
+typedef struct Node{
+	Pixel_t pixel;
+	Node_t next;
+	Node_t prev;
+	int index;	
+} Node_t;
+
+typedef struct ImageChunk{
+	int size;
+	Node_t head;
+	Node_t tail;
+} ImageChunk_t;
