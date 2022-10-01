9062e4554616900f74865ad6d4ec906c4556c8d3
NachoGranados <ignaciogramar@gmail.com>
2022-10-01 01:47:11 -0600

    Adding stats shared memory definition

---
+++ visualizer/structs.h
@@ -0,0 +1,23 @@
+typedef struct Pixel {
+    int value;
+    int index;
+    int metadata_id;
+    int dirty_bit;
+} Pixel_t;
+
+typedef struct Node Node_t;
+
+typedef struct Node {
+    Pixel_t* pixel;
+    Node_t* next;
+    int index;
+} Node_t;
+
+typedef struct Stat {
+    int total_used_memory;
+    int total_semaphore_wait;
+    int total_data_transfered;
+    int total_kernel_time;
+    int total_pixels_175;
+    int total_instances;
+} Stat_t;
---
+++ visualizer/visualizer.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/mman.h>
+
+#include <structs.h>
+#include <constants.h>
+
+int main() {
+
+    int fd = shm_open(SM_STATS, O_RDWR | O_EXCL, S_IRUSR | S_IWUSR);
+
+    if(fd == -1) {
+
+        fd = shm_open(SM_STATS, O_RDWR | O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
+
+        ftruncate(fd, sizeof(Stat_t));
+
+    }
+
+    int *stat_ptr = mmap(NULL, sizeof(Stat_t), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+
+    
+
+
+
+
+
+
+
+
+    munmap(stat_ptr, sizeof(Stat_t));
+
+    unlink(SM_STATS);
+
+    close(fd);    
+
+    return 0;
+
+}
+
+    
+
+
+
+
+
+
+
